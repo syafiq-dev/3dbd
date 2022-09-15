f1d927bfe4f9fba8a735fd46f1107f7f646a35f8
shiyapiku-dev <syafq.dev@gmail.com>
2022-09-15 17:15:21 +0800

    Heart done

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import * as dat from 'lil-gui'
+import { MathUtils } from 'three'
 
 /**
  * Base
@@ -14,38 +15,48 @@ const gui = new dat.GUI()
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+// Colors
+const pinkColor = new THREE.Color( 'pink' );
+
 // Scene
 const scene = new THREE.Scene()
+scene.background = pinkColor
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const matcap1 = textureLoader.load('textures/matcaps/1.png')
-const matcap2 = textureLoader.load('textures/matcaps/2.png')
-const matcap3 = textureLoader.load('textures/matcaps/3.png')
-const matcap4 = textureLoader.load('textures/matcaps/4.png')
-const matcap5 = textureLoader.load('textures/matcaps/5.png')
-const matcap6 = textureLoader.load('textures/matcaps/6.png')
-const matcap7 = textureLoader.load('textures/matcaps/7.png')
-const matcap8 = textureLoader.load('textures/matcaps/8.png')
+const pink1 = textureLoader.load('textures/matcaps/pink1.png')
+const pink2 = textureLoader.load('textures/matcaps/pink2.png')
+const purple1 = textureLoader.load('textures/matcaps/purple1.png')
+const cyan1 = textureLoader.load('textures/matcaps/cyan1.png')
+// const matcap1 = textureLoader.load('textures/matcaps/1.png')
+// const matcap2 = textureLoader.load('textures/matcaps/2.png')
+// const matcap3 = textureLoader.load('textures/matcaps/3.png')
+// const matcap4 = textureLoader.load('textures/matcaps/4.png')
+// const matcap5 = textureLoader.load('textures/matcaps/5.png')
+// const matcap6 = textureLoader.load('textures/matcaps/6.png')
+// const matcap7 = textureLoader.load('textures/matcaps/7.png')
+// const matcap8 = textureLoader.load('textures/matcaps/8.png')
 
 /**
  * Fonts
  */
 const fontLoader = new FontLoader()
 
-
 fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
     (font) =>
     {
         // Material
-        const material = new THREE.MeshMatcapMaterial({ matcap: matcap3 })
+        const purpleMat = new THREE.MeshMatcapMaterial({ matcap: purple1 })
+        const pinkMat1 = new THREE.MeshMatcapMaterial({ matcap: pink1 })
+        const pinkMat2 = new THREE.MeshMatcapMaterial({ matcap: pink2 })
+        const cyanMat = new THREE.MeshMatcapMaterial({ matcap: cyan1 })
 
         // Text
         const textGeometry = new TextGeometry(
-            'syafiq.art',
+            't e x t',
             {
                 font: font,
                 size: 0.5,
@@ -60,27 +71,86 @@ fontLoader.load(
         )
         textGeometry.center()
 
-        const text = new THREE.Mesh(textGeometry, material)
+        const text = new THREE.Mesh(textGeometry, purpleMat)
         scene.add(text)
 
         // Donuts
         const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+        // Ruby
+        const rubyGeometry = new THREE.IcosahedronGeometry(1,0)
+        /*
+            Heart
+        */
+            const x = 0, y = 0;
 
-        for(let i = 0; i < 300; i++)
+            const heartShape = new THREE.Shape();
+            
+            heartShape.moveTo( x + 5, y + 5 );
+            heartShape.bezierCurveTo( x + 5, y + 5, x + 4, y, x, y );
+            heartShape.bezierCurveTo( x - 6, y, x - 6, y + 7,x - 6, y + 7 );
+            heartShape.bezierCurveTo( x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19 );
+            heartShape.bezierCurveTo( x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7 );
+            heartShape.bezierCurveTo( x + 16, y + 7, x + 16, y, x + 10, y );
+            heartShape.bezierCurveTo( x + 7, y, x + 5, y + 5, x + 5, y + 5 );
+        const extrudeSettings = {
+            steps: 1,
+            depth: 2,
+            bevelEnabled: true,
+            bevelThickness: 2,
+            bevelSize: 4,
+            bevelOffset: -4,
+            bevelSegments: 4
+        };
+        const heartGeometry = new THREE.ExtrudeGeometry( heartShape, extrudeSettings );
+
+        const minS = .01, maxS = .02
+
+        for(let i = 0; i < 50; i++)
         {
-            const donut = new THREE.Mesh(donutGeometry, material)
+            const donut = new THREE.Mesh(heartGeometry, pinkMat1)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            // donut.rotation.x = Math.random() * Math.PI
+            // donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random() / -30
+
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
+        for(let i = 0; i < 150; i++)
+        {
+            const donut = new THREE.Mesh(heartGeometry, pinkMat2)
             donut.position.x = (Math.random() - 0.5) * 10
             donut.position.y = (Math.random() - 0.5) * 10
             donut.position.z = (Math.random() - 0.5) * 10
             donut.rotation.x = Math.random() * Math.PI
             donut.rotation.y = Math.random() * Math.PI
-            const scale = Math.random()
+            const scale = -MathUtils.randFloat(minS,maxS)
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+        }
+        for(let i = 0; i < 150; i++)
+        {
+            const donut = new THREE.Mesh(heartGeometry, cyanMat)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = -MathUtils.randFloat(minS,maxS)
             donut.scale.set(scale, scale, scale)
 
             scene.add(donut)
         }
     }
 )
+
+function randomNumber(min, max) { 
+    return Math.floor(Math.random() * (max - min) + min);
+} 
 
 /**
  * Sizes
@@ -148,3 +218,4 @@ const tick = () =>
 }
 
 tick()
+
